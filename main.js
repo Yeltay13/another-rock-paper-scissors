1d78323b1d7e5b219e45f6c9a2182e196d4060b8
Yeltay Bayandinov <bayandinov13@gmail.com>
2022-07-04 21:43:51 +0600

    fixes

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-const buttons = document.querySelectorAll("button");
+let playerScore = 0;
+let computerScore = 0;
+const images = document.querySelectorAll("img");
 
 function computerRandom() {
   let choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function disableImages() {
+  images.forEach((image) => {
+    image.disabled = true;
+  });
 }
 
 function game(playerSelection) {
@@ -13,18 +21,52 @@ function game(playerSelection) {
     (playerSelection == "scissors" && computerSelection == "paper") ||
     (playerSelection == "paper" && computerSelection == "rock")
   ) {
-    result = "Human Wins";
+    playerScore += 1;
+    result =
+      "You win! " +
+      playerSelection +
+      " beats " +
+      computerSelection +
+      "<br><br>Player score: " +
+      playerScore +
+      "<br>Computer score: " +
+      computerScore;
+
+    if (playerScore == 5) {
+      result += "<br><br>You won the game! Reload the page to play again";
+      disableImages();
+    }
   } else if (playerSelection == computerSelection) {
-    result = "It's a tie";
+    result =
+      "It's a tie. You both chose " +
+      playerSelection +
+      "<br><br>Player score: " +
+      playerScore +
+      "<br>Computer score: " +
+      computerScore;
   } else {
-    result = "Computer wins";
+    computerScore += 1;
+    result =
+      "You lose! " +
+      computerSelection +
+      " beats " +
+      playerSelection +
+      "<br><br>Player score: " +
+      playerScore +
+      "<br>Computer score: " +
+      computerScore;
+
+    if (computerScore == 5) {
+      result += "<br><br>I won the game! Reload the page to play again";
+      disableImages();
+    }
   }
   document.getElementById("result").innerHTML = result;
   return;
 }
 
-buttons.forEach((button) => {
-  button.addEventListener("click", function () {
-    game(button.value);
+images.forEach((image) => {
+  image.addEventListener("click", function () {
+    game(image.className);
   });
 });
